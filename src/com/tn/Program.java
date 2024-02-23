d3ec888f0f6499b031e1578730b8671b2c22fffd
Hoang Ngoc Quan <hoangnocquan22092004@gmail.com>
2024-02-23 20:10:48 +0700

    run5

--- src/com/tn/Program.java
+++ src/com/tn/Program.java
@@ -1,5 +1,10 @@
 package com.tn;
 
 public class Program {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git 2");
+        System.out.println("hello git 2");
 
+    }
 }
---
+++ src/com/tn/Program2.java
@@ -0,0 +1,12 @@
+package com.tn;
+
+public class Program2 {
+    public static void main(String[] args) {
+        System.out.println("run 1")
+        System.out.println("run 2")
+        System.out.println("run 3")
+        System.out.println("run 4")
+        System.out.println("run 5")
+
+    }
+}
